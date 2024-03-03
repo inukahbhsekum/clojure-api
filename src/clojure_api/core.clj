88122a38ba3d4288471182e4da27f9b06d0e8b8f
Mukesh Bhakuni <mukeshbhakuni.almora@gmail.com>
2024-03-03 14:43:11 +0530

    Add tests for db migrations
    + Add tests for migrations support added to
      the datasource component

--- src/clojure_api/core.clj
+++ src/clojure_api/core.clj
@@ -3,66 +3,38 @@
             [clojure-api.components.in-memory-state-component :as in-memory-state-component]
             [clojure-api.components.pedestal-component :as pedestal-component]
             [clojure-api.config :as config]
-            [clojure.string :as str]
             [com.stuartsierra.component :as component]
-            [hikari-cp.core :as cp]
             [io.pedestal.log :as log]
             [next.jdbc.connection :as connection])
   (:import (com.zaxxer.hikari HikariDataSource)
-           (java.net URI)
            (org.flywaydb.core Flyway)))
 
 
-(defn- db-info-from-url
+(defn datasource-component
   [config]
-  (let [db-spec (:db-spec config)
-        db-uri (URI. (:jdbcUrl db-spec))]
-    (println "db-url: " db-uri)
-    {:username      (:username db-spec)
-     :password      (:password db-spec)
-     :port-number   "5432"
-     :database-name (str/replace-first (.getPath db-uri) "/" "")
-     :server-name   (.getHost db-uri)}))
-
-
-(defn- datasource-options
-  [config]
-  (merge (db-info-from-url config)
-         {:auto-commit        true
-          :read-only          false
-          :adapter            "postgresql"
-          :connection-timeout 30000
-          :validation-timeout 5000
-          :idle-timeout       600000
-          :max-lifetime       1800000
-          :minimum-idle       10
-          :maximum-pool-size  20
-          :pool-name          "db-pool"
-          :register-mbeans    false}))
+  (let [init-function (fn [datasource]
+                        (log/info "Running database init" datasource)
+                        (.migrate
+                          (.. (Flyway/configure)
+                              (dataSource datasource)
+                              (locations (into-array String ["classpath:database/migrations"]))
+                              (table "schema_version")
+                              (load))))]
+    (connection/component HikariDataSource
+                          (assoc (:db-spec config) :init-fn init-function))))
 
 
 (defn clojure-api-system
   [config]
-  (let [data-source (delay (-> config
-                               datasource-options
-                               cp/make-datasource))]
-    (component/system-map
-      :example-component (example-component/new-example-component config)
-      :in-memory-state-component (in-memory-state-component/new-in-memory-state-component config)
-      :data-source (connection/component HikariDataSource
-                                         (assoc (:db-spec config) :init-fn (fn [datasource]
-                                                                             (log/info "Running database init" datasource)
-                                                                             (.migrate
-                                                                               (.. (Flyway/configure)
-                                                                                   (dataSource datasource)
-                                                                                   (locations (into-array String ["classpath:database/migrations"]))
-                                                                                   (table "schema_version")
-                                                                                   (load))))))
-      :pedestal-component (component/using
-                            (pedestal-component/new-pedestal-component config)
-                            [:example-component
-                             :data-source
-                             :in-memory-state-component]))))
+  (component/system-map
+    :example-component (example-component/new-example-component config)
+    :in-memory-state-component (in-memory-state-component/new-in-memory-state-component config)
+    :data-source (datasource-component config)
+    :pedestal-component (component/using
+                          (pedestal-component/new-pedestal-component config)
+                          [:example-component
+                           :data-source
+                           :in-memory-state-component])))
 
 
 (defn -main
